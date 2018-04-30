6fdc802ad284a0036d4efd8cebe4de26cbea0086
levy <levy.ma.1984@gmail.com>
2018-04-30 19:55:41 +0800

    整理项目

---
+++ odin-runes/src/main/java/com/levy/runes/RunesApplication.java
@@ -0,0 +1,12 @@
+package com.levy.runes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunesApplication.class, args);
+	}
+}
---
+++ odin-runes/src/main/java/com/levy/runes/kafka/Listener.java
@@ -0,0 +1,22 @@
+package com.levy.runes.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+public class Listener {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     *
+     * @param record
+     */
+    @KafkaListener(topics = {"test"})
+    public void listen(ConsumerRecord<?, ?> record) {
+//        logger.info("kafka的key: " + record.key());
+//        logger.info("kafka的value: " + record.value().toString());
+    }
+
+}
